3cad71ecb9c3d9035add354ef031733ae199e099
na-rachl <ma.ra@northeastern.edu>
2023-12-04 11:06:50 -0500

    added heroku backend

--- src/project/users/account.js
+++ src/project/users/account.js
@@ -43,42 +43,6 @@ function Account() {
             {account && (
                 <div>
                     <div>
-                    {/*    <input value={account.password}*/}
-                    {/*           onChange={(e) => setAccount({ ...account,*/}
-                    {/*               password: e.target.value })}/>*/}
-                    {/*    <select onChange={(e) => setAccount({ ...account,*/}
-                    {/*        role: e.target.value })}>*/}
-                    {/*        <option value="USER">User</option>*/}
-                    {/*        <option value="ADMIN">Admin</option>*/}
-                    {/*        <option value="FACULTY">Faculty</option>*/}
-                    {/*        <option value="STUDENT">Student</option>*/}
-                    {/*    </select>*/}
-
-                    {/*</div>*/}
-                    {/*<div>*/}
-                    {/*    <input value={account.firstName}*/}
-                    {/*           placeholder="firstname"*/}
-                    {/*           onChange={(e) => setAccount({ ...account,*/}
-                    {/*               firstname: e.target.value })}/>*/}
-                    {/*</div>*/}
-                    {/*<div>*/}
-                    {/*    <input value={account.lastName}*/}
-                    {/*           placeholder="lastnmae"*/}
-                    {/*           onChange={(e) => setAccount({ ...account,*/}
-                    {/*               lastName: e.target.value })}/>*/}
-                    {/*</div>*/}
-                    {/*<div>*/}
-                    {/*    <input value={account.dob}*/}
-                    {/*           placeholder="date of birth"*/}
-                    {/*           onChange={(e) => setAccount({ ...account,*/}
-                    {/*               dob: e.target.value })}/>*/}
-                    {/*</div>*/}
-                    {/*<div>*/}
-                    {/*    <input value={account.email}*/}
-                    {/*           placeholder="email"*/}
-                    {/*           onChange={(e) => setAccount({ ...account,*/}
-                    {/*               email: e.target.value })}/>*/}
-                    {/*</div>*/}
                         <select className="account-inputs form-select"
                                 value={account && account.role ? account.role : "USER"}
                                 onChange={(e) => setAccount({
